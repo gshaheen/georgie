c7e48192e8b50654e301a29f0bde341805997817
George Shaheen <gshaheen@gmail.com>
2021-10-07 17:29:48 -0700

    adding commands

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 
 import cli from "commander";
+import posts from "./commands/posts.js";
 
 cli.description("Access the JSON Placeholder API");
-cli.name("jsonp");
+cli.name("georgie");
+cli.usage("<command>");
+cli.addHelpCommand(false);
+cli.helpOption(false);
+
+cli
+  .command("posts")
+  .argument("[postId]", "ID of post you'd like to retrieve.")
+  .option("-p, --pretty", "Pretty-print output from the API.")
+  .description(
+    "Retrieve a list of all posts or one post by passing the post ID (e.g., posts 1)."
+  )
+  .action(posts);
 
 cli.parse(process.argv);
