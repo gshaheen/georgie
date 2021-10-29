af2f0c41aaa57c680ee5d9aa741e813592a2d612
George Shaheen <gshaheen@gmail.com>
2021-10-29 08:16:34 -0700

    starting with blessed

--- commands/watch.js
+++ commands/watch.js
@@ -6,20 +6,69 @@ import blessed from "blessed";
 export default (address, options) => {
   const web3 = createAlchemyWeb3(PROVIDER_URL);
 
-  async function watch () {
-      try { 
-        const data = await web3.alchemy.getAssetTransfers({
-          fromAddress:address,
-          fromBlock:7777777
-        });
-        
-        var strData = JSON.stringify(data);
+  async function watch() {
+    try {
+      const data = await web3.alchemy.getAssetTransfers({
+        fromAddress: address,
+        fromBlock: 7777777,
+        maxCount: 5
+      });
 
-        return console.log(data);
+      let headers = ['blocknum','hash','from','to','value','erc721token','asset','category','raw contract'];
+      let transactionData = [];
 
-      } catch (error) { 
-        return console.log(error.name + ": " + error.message);
-      }
-    };
+      for (let i = 0; i < data.transfers.length; i++) {
+        transactionData.push(data.transfers[i]);
+      };
+console.log(transactionData);
+      for (let i = 0; i < transactionData.length; i++) {
+        console.log(transactionData[i].blockNum + ' ' + transactionData[i].hash);
+      };
+
+      //return console.log(data);
+
+      // // Create a screen object.
+      // var screen = blessed.screen({
+      //   smartCSR: true
+      // });
+
+      // screen.title = 'ethereum activity';
+
+      // // Create a box perfectly centered horizontally and vertically.
+      // var box = blessed.box({
+      //   top: 'center',
+      //   left: 'center',
+      //   width: '100%',
+      //   height: '100%',
+      //   content: strData,
+      //   }
+      // );
+
+      // // var table = blessed.table();
+      // // table.setData([
+      // //   [ 'Animals',  'Foods'  ],
+      // //   [ 'Elephant', 'Apple'  ],
+      // //   [ 'Bird',     'Orange' ]
+      // // ]);
+
+      // // Append our box to the screen.
+      // screen.append(box);
+      // // box.append(table);
+
+      // // Quit on Escape, q, or Control-C.
+      // screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+      //   return process.exit(0);
+      // });
+
+      // // Focus our element.
+      // box.focus();
+
+      // // Render the screen.
+      // screen.render();
+
+    } catch (error) {
+      return console.log(error.name + ": " + error.message);
+    }
+  };
   watch();
 };
